98d9db475826f0579d71c133ee76840cd791ecf8
Zoltan Farkas <progfarkas@gmail.com>
2024-05-08 14:24:49 +0200

    use ecs json format

--- loggerjson.go
+++ loggerjson.go
@@ -3,21 +3,30 @@ package traefiklogger
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 // JSONHTTPLogger a JSON logger implementation.
 type JSONHTTPLogger struct {
-	clock  LoggerClock
-	logger *log.Logger
-	writer LogWriter
+	clock   LoggerClock
+	logger  *log.Logger
+	writer  LogWriter
+}
+
+type Origin struct {
+	FileName string `json:"file.name,omitempty"`
+	FileLine int    `json:"file.line,omitempty"`
 }
 
 func (jhl *JSONHTTPLogger) print(record *LogRecord) {
 	logData := struct {
+		Level                 string              `json:"log.level,omitempty"`
+		Time                  string              `json:"@timestamp"`
+		Origin                Origin              `json:"log.origin"`
+		Message               string              `json:"message,omitempty"`
 		System                string              `json:"system,omitempty"`
-		Time                  string              `json:"time"`
 		RemoteAddr            string              `json:"remoteAddr,omitempty"`
 		Method                string              `json:"method"`
 		URL                   string              `json:"url"`
@@ -29,9 +38,13 @@ func (jhl *JSONHTTPLogger) print(record *LogRecord) {
 		ResponseHeaders       map[string][]string `json:"responseHeaders,omitempty"`
 		ResponseContentLength int                 `json:"responseContentLength"`
 		ResponseBody          string              `json:"responseBody,omitempty"`
+		EcsVersion            string              `json:"ecs.version,omitempty"`
 	}{
-		System:                record.System,
+		Level:                 "info",
 		Time:                  jhl.clock.Now().UTC().Format("2006-01-02T15:04:05.999Z07:00"),
+		Origin:                Origin{FileName: "loggerjson.go", FileLine: 1},
+		Message:               fmt.Sprintf("%s %s %s %d", record.Method, record.URL, record.Proto, record.StatusCode),
+		System:                record.System,
 		RemoteAddr:            record.RemoteAddr,
 		Method:                record.Method,
 		URL:                   record.URL,
@@ -43,6 +56,7 @@ func (jhl *JSONHTTPLogger) print(record *LogRecord) {
 		ResponseHeaders:       record.ResponseHeaders,
 		ResponseContentLength: record.ResponseContentLength,
 		ResponseBody:          record.ResponseBody.String(),
+		EcsVersion:            "1.6.0",
 	}
 
 	logBytes, err := json.Marshal(logData)
